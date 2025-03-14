7593d20fda69fc91a8457421d34e37af32bcd664
Alexandre Junior <alejunior.dev@gmail.com>
2025-03-14 00:41:16 -0300

    feat: add swagger configuration

--- router/routes.go
+++ router/routes.go
@@ -1,15 +1,28 @@
 package router
 
 import (
+	docs "github.com/alexandrejuniorc/gopportunities/docs"
 	"github.com/alexandrejuniorc/gopportunities/handler"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
+	BASE_PATH := "/api/v1"
+
+	// programmatically set swagger info
+	docs.SwaggerInfo.Title = "Gopportunities API"
+	docs.SwaggerInfo.Description = "This is a sample server to create job opportunities using Go."
+	docs.SwaggerInfo.Version = "1.0"
+	// docs.SwaggerInfo.Host = "petstore.swagger.io"
+	docs.SwaggerInfo.BasePath = BASE_PATH
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+
 	// Initialize handler
 	handler.InitializeHandler()
 
-	v1 := router.Group("/api/v1")
+	v1 := router.Group(BASE_PATH)
 	{
 		v1.GET("/openings", handler.GetAllOpeningsHandler)
 		v1.GET("/opening", handler.GetOpeningHandler)
@@ -17,4 +30,7 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 	}
+
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
